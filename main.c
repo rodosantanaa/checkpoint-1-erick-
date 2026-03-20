ed83a5d50be3399cd9bccb0ffb4ead79e8736149
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-20 11:25:04 -0300

    adicionado nova linha printf

--- main.c
+++ main.c
@@ -4,6 +4,8 @@ int main(){
     printf("Ola Olavo!");
     printf("\n");
     printf("Ola FIAP!");
+        printf("\n");
+    printf("Seja bem vindo a instituição de ensino FIAP");
 
     return 0;
 
