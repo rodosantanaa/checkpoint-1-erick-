79aba859e09eaa9f9f5031fa35139ca3bed96380
Laboratório FIAP <labsfiap@fiap.com.br>
2026-03-20 11:44:10 -0300

    Adicionado o código - Marcelo

--- main.c
+++ main.c
@@ -4,10 +4,12 @@ int main(){
     printf("Ola Olavo!");
     printf("\n");
     printf("Ola FIAP!");
-        printf("\n");
+    printf("\n");
     printf("Seja bem vindo a instituição de ensino FIAP");
     printf("\n");
     printf("Deus me salve desse sofrimento");
+    printf("Olá Checkpoint 1 do Erick! :)");
+    printf("\n");
+    printf("Seja bem-vindo aluno Marcelo!");
     return 0;
-
 }
